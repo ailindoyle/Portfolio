836abc6ddea92f2515f7a89ffca501e8bf0dd117
ailindoyle <caitlin.doyle@mobile-life.com>
2018-03-19 16:09:12 +0000

    form layout completed

--- admin.php
+++ admin.php
@@ -53,3 +53,41 @@
         <input type="submit" value="Save">
     </form>
 </div>
+<div class="container">
+    <h2>SKILLS</h2>
+    <form method="post" action="form_insert.php">
+        Skills Name:<br>
+        <input type="text" name="skillsName"><br>
+        Alternative Image Text:<br>
+        <input type="text" name="alternativeText"><br>
+        Image Source:<br>
+        <input type="text" name="imageSource"><br>
+        <input type="submit" value="Save">
+    </form>
+</div>
+<div class="container">
+    <h2>PORTFOLIO</h2>
+    <form method="post" action="form_insert.php">
+        Project Name:<br>
+        <input type="text" name="projectName"><br>
+        Project Description:<br>
+        <input type="text" name="projectDescription"><br>
+        Link to project:<br>
+        <input type="text" name="link"><br>
+        Image Source:<br>
+        <input type="text" name="imageSource"><br>
+        Alternative Image Text:<br>
+        <input type="text" name="alternativeText"><br>
+        <input type="submit" value="Save">
+    </form>
+</div>
+<div class="container">
+    <h2>CONTACT</h2>
+    <form method="post" action="form_insert.php">
+        Descriptiom:<br>
+        <input type="text" name="description"><br>
+        Email address:<br>
+        <input type="text" name="email"><br>
+        <input type="submit" value="Save">
+    </form>
+</div>
