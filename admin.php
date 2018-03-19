b197ac5d6033cbf07d6881c3da40cbbb114e9918
ailindoyle <caitlin.doyle@mobile-life.com>
2018-03-19 15:58:34 +0000

    featured projects form layout

--- admin.php
+++ admin.php
@@ -14,14 +14,30 @@
 </head>
 <body>
 <div class="container">
-    <h2 >HOME</h2>
-    <form method="post" action="sql_insert.php">
+    <h2>HOME</h2>
+    <form method="post" action="form_insert.php">
         Header Top:<br>
         <input type="text" name="headerTop"><br>
         Header Bottom:<br>
         <input type="text" name="headerBottom"><br>
         Summary:<br>
         <input type="text" name="summary"><br>
+        <input type="submit" value="Save">
+    </form>
+</div>
+<div class="container">
+    <h2>FEATURED PROJECTS</h2>
+    <form method="post" action="form_insert.php">
+        Project Name:<br>
+        <input type="text" name="projectName"><br>
+        Project Description:<br>
+        <input type="text" name="projectDescription"><br>
+        Link to project:<br>
+        <input type="text" name="link"><br>
+        Image Source:<br>
+        <input type="text" name="imageSource"><br>
+        Alternative Image Text:<br>
+        <input type="text" name="alternativeText"><br>
         <input type="submit" value="Save">
     </form>
 </div>
