a842bd8b9f7329452f94d0ec967e2c5991486182
ailindoyle <caitlin.doyle@mobile-life.com>
2018-03-22 09:21:42 +0000

    login framework added

---
+++ account.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$correctUsername = 'Chicken Nuggets';
+$correctPassword = 'smallMonkey';
+
+function checkCredentials ($username, $password, $correctUsername, $correctPassword) {
+
+    if ($username==$correctUsername&&$password==$correctPassword || $_SESSION['loggedIn']==TRUE) {
+        $_SESSION['loggedIn']=TRUE;
+        header('Location: admin.php');
+        exit;
+    } else {
+        $_SESSION['loggedIn']=FALSE;
+        header('Location: index.php');
+        exit;
+    }
+
+}
---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+if ($_SESSION['loggedIn']) {
+    header('Location: account.php');
+    exit;
+}
+
+
+?>
+
+<form method="post" action="account.php">
+    Username:<br>
+    <input type="text" name="username"><br>
+    Password:<br>
+    <input type="text" name="password">
+    <input type="submit">
+</form>
