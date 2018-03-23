6b7a35703d564417c6be8bccb8f858affcaa7fba
ailindoyle <caitlin.doyle@mobile-life.com>
2018-03-23 13:42:32 +0000

    updated db structure file, unused query removed

--- home_insert.php
+++ home_insert.php
@@ -5,8 +5,6 @@ include 'settings.php';
 $db = new PDO($dsn, $user);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query= $db->prepare("INSERT INTO `home` (`headerTop`,`headerBottom`, `summary`) VALUES (:headerTop, :headerBottom, :summary);");
-
 insertHome($db, $_POST);
 
 header('Location: admin_home.php');
